395aeddcfc2d5c652babff0d7fd2bb7d64d904f8
lyx9799 <sld.lyx@gmail.com>
2021-06-16 10:26:07 +0800

    Update kaldi_data_preparation.py

--- youtube/kaldi_data_preparation.py
+++ youtube/kaldi_data_preparation.py
@@ -5,11 +5,12 @@ import os
 class Kaldi_Data_Preparation:
     kaldi_text_format = "{v_id}_{begin}-{end} {content}"
     kaldi_seg_format = "{v_id}_{begin}-{end} {v_id} {seg_begin} {seg_end}"
-    language_codes = ['zh-TW']
+    language_codes = ['zh-CN', 'zh-Hans', 'zh-TW']
 
     def __init__(self, url):
         self.source = YouTube(url)
         self.v_id = extract.video_id(url)
+        self.lc = None
 
     @staticmethod
     def parse_time_lazy(time_str):
@@ -26,7 +27,7 @@ class Kaldi_Data_Preparation:
         video = self.source.streams.filter(only_audio=True).first()
 
         # download the file
-        out_file = video.download(output_path=destination)
+        out_file = video.download(output_path=destination.format(lc=self.lc))
 
         # save the file
         base, ext = os.path.splitext(out_file)
@@ -34,10 +35,16 @@ class Kaldi_Data_Preparation:
         os.rename(out_file, new_file)
 
         # result of success
-        print(self.title + " has been successfully downloaded.")
+        print(self.source.title + " has been successfully downloaded.")
 
-    def download_caption(self, text_file, seg_file, language_code='zh-TW'):
-        caption = self.source.captions.get_by_language_code(language_code)
+    def download_caption(self, text_file, seg_file):
+        avail_lans = self.source.captions.keys()
+        for lan in self.language_codes:
+            if lan in avail_lans:
+                self.lc = lan
+                break
+        print(self.lc)
+        caption = self.source.captions.get_by_language_code(self.lc)
         caption_str = (caption.generate_srt_captions()).split('\n')
 
         kaldi_text = ''
@@ -64,23 +71,30 @@ class Kaldi_Data_Preparation:
         print(kaldi_text)
         print(kaldi_seg)
 
-        with open(text_file, "a+") as t_file:
+        with open(text_file.format(lc=self.lc), "a+") as t_file:
             t_file.write(kaldi_text)
 
-        with open(seg_file, "a+") as s_file:
+        with open(seg_file.format(lc=self.lc), "a+") as s_file:
             s_file.write(kaldi_seg)
+
+
+def create_new_lan_folders(languages):
+    for l in languages:
+        os.makedirs('data/' + l, exist_ok=True)
+        os.makedirs('data/' + l + '/videos', exist_ok=True)
 
 
 if __name__ == '__main__':
     urls = ['https://www.youtube.com/watch?v=N0zhdMwD2Z8&list=PL-2gsH0BOSxGwtLx6qnEH3Av7HMhhzbha&ab_channel=TEDxTaipeiTEDxTaipeiVerified',
             'https://www.youtube.com/watch?v=5YIT-byVUOY&list=PL-2gsH0BOSxGwtLx6qnEH3Av7HMhhzbha&index=4&ab_channel=TEDxTalksTEDxTalksVerified']
-    videos_destination = 'data/videos/'
-    segements_file = 'data/segments'
-    text_file = 'data/text'
+
+    videos_destination = 'data/{lc}/videos/'
+    segements_file = 'data/{lc}/segments'
+    text_file = 'data/{lc}/text'
+
+    create_new_lan_folders(['zh-CN', 'zh-Hans', 'zh-TW'])
 
     for url in urls:
         kaldi_data_prep = Kaldi_Data_Preparation(url)
-        # kaldi_data_prep.download_audio(destination=videos_destination)
-        kaldi_data_prep.download_caption(text_file=text_file, seg_file=segements_file, language_code='zh-TW')
-
-
+        kaldi_data_prep.download_caption(text_file=text_file, seg_file=segements_file)
+        kaldi_data_prep.download_audio(destination=videos_destination)
