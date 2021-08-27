63f4b961bad9771ebdd1c43160e9dcf75e303a87
lyx9799 <sld.lyx@gmail.com>
2021-08-27 16:38:14 +0800

    Update kaldi_data_preparation.py

--- youtube/kaldi_data_preparation.py
+++ youtube/kaldi_data_preparation.py
@@ -1,11 +1,12 @@
-from pytube import YouTube, extract
+from pytube import YouTube, Playlist, extract
 import os
 
 
 class Kaldi_Data_Preparation:
     kaldi_text_format = "{v_id}_{begin}-{end} {content}"
     kaldi_seg_format = "{v_id}_{begin}-{end} {v_id} {seg_begin} {seg_end}"
-    language_codes = ['zh-CN', 'zh-Hans', 'zh-TW']
+    language_codes = ['zh-Hant', 'zh', 'zh-Hans', 'zh-CN', 'zh-TW']
+    log_file = 'data/log'
 
     def __init__(self, url):
         self.source = YouTube(url)
@@ -23,27 +24,41 @@ class Kaldi_Data_Preparation:
         return str(secs + mins*60 + hrs*3600) + '.' + ms
 
     def download_audio(self, destination='.'):
-        # extract only audio
-        video = self.source.streams.filter(only_audio=True).first()
-
         # download the file
-        out_file = video.download(output_path=destination.format(lc=self.lc))
+        for _ in range(5):
+            try:
+                # extract only audio
+                video = self.source.streams.filter(only_audio=True).first()
+                out_file = video.download(output_path=destination.format(lc=self.lc))
+                break
+            except Exception as e:
+                print(e)
 
         # save the file
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = destination.format(lc=self.lc) + self.v_id + '.mp3'
         os.rename(out_file, new_file)
 
         # result of success
         print(self.source.title + " has been successfully downloaded.")
+        with open(self.log_file, "a") as file_object:
+            file_object.write("audio " + self.v_id + " 200\n")
 
     def download_caption(self, text_file, seg_file):
-        avail_lans = self.source.captions.keys()
+        for _ in range(5):
+            try:
+                avail_lans = self.source.captions.keys()
+                break
+            except Exception as e:
+                print(e)
+
         for lan in self.language_codes:
             if lan in avail_lans:
                 self.lc = lan
                 break
-        print(self.lc)
+
+        if not self.lc:
+            return False
+
         caption = self.source.captions.get_by_language_code(self.lc)
         caption_str = (caption.generate_srt_captions()).split('\n')
 
@@ -77,6 +92,11 @@ class Kaldi_Data_Preparation:
         with open(seg_file.format(lc=self.lc), "a+") as s_file:
             s_file.write(kaldi_seg)
 
+        with open(self.log_file, "a") as file_object:
+            file_object.write("caption " + self.v_id + " 200\n")
+
+        return True
+
 
 def create_new_lan_folders(languages):
     for l in languages:
@@ -85,16 +105,34 @@ def create_new_lan_folders(languages):
 
 
 if __name__ == '__main__':
-    urls = ['https://www.youtube.com/watch?v=N0zhdMwD2Z8&list=PL-2gsH0BOSxGwtLx6qnEH3Av7HMhhzbha&ab_channel=TEDxTaipeiTEDxTaipeiVerified',
-            'https://www.youtube.com/watch?v=5YIT-byVUOY&list=PL-2gsH0BOSxGwtLx6qnEH3Av7HMhhzbha&index=4&ab_channel=TEDxTalksTEDxTalksVerified']
+    pl = 'https://www.youtube.com/playlist?list=PLzu0ghz97t3ROyBA2-7f5thrqrOYN_g3r'
+    urls = Playlist(pl).video_urls
 
     videos_destination = 'data/{lc}/videos/'
     segements_file = 'data/{lc}/segments'
     text_file = 'data/{lc}/text'
 
-    create_new_lan_folders(['zh-CN', 'zh-Hans', 'zh-TW'])
+    # cc langs
+    # {'zh-Hant': <Caption lang="Chinese (Traditional)" code="zh-Hant">,
+    #   'zh': <Caption lang="Chinese" code="zh">,
+    #   'zh-Hans': <Caption lang="Chinese (Simplified)" code="zh-Hans">,
+    #   'zh-CN': <Caption lang="Chinese (China)" code="zh-CN">,
+    #   'zh-TW': <Caption lang="Chinese (Taiwan)" code="zh-TW">
 
-    for url in urls:
+    create_new_lan_folders(['zh-Hant', 'zh', 'zh-Hans', 'zh-CN', 'zh-TW'])
+
+    for url in list(urls)[12:]:
         kaldi_data_prep = Kaldi_Data_Preparation(url)
-        kaldi_data_prep.download_caption(text_file=text_file, seg_file=segements_file)
-        kaldi_data_prep.download_audio(destination=videos_destination)
+        success = kaldi_data_prep.download_caption(text_file=text_file, seg_file=segements_file)
+        if success:
+            kaldi_data_prep.download_audio(destination=videos_destination)
+
+    # redownload url
+    # url = list(urls)[42]
+    # kaldi_data_prep = Kaldi_Data_Preparation(url)
+    # kaldi_data_prep.download_audio(destination=videos_destination)
+
+
+# url = 'https://www.youtube.com/watch?v=5YIT-byVUOY&list=PL-2gsH0BOSxGwtLx6qnEH3Av7HMhhzbha&index=4&ab_channel=TEDxTalksTEDxTalksVerified'
+# source = YouTube(url)
+# source.captions.all
